cf396aeb26bb77836299f60a39837838c24408b6
themarkieone <themarkieone@gmail.com>
2017-04-16 16:19:57 +0200

    Added loop to add as many grids as the user wants.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,9 +1,10 @@
 //Make a grid out of divs, append with jQuery//
 $(document).ready(function() {
-		var grid = $("<div class="class"></div>");
-		for(var x = 0; x < 2; x+){
-			for(var y= 0; y < 2; y++){
-				grid.appendTo('container');
-			}
-		}
+	var num = prompt("How many grids would you like to play with?");
+	for(var i = 0; i < num; i++) {
+		$("<div class='unit'></div>").appendTo(".container");
+	}
+
 });
+
+
