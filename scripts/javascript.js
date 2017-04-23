9a05767c3fc5e045c2b33b53dbb66b573c1a2adf
themarkieone <themarkieone@gmail.com>
2017-04-23 18:40:54 +0200

    Fixed grid structure to center and black hover effect.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,10 +1,9 @@
 //Make a grid out of divs, append with jQuery//
 $(document).ready(function() {
-	var num = prompt("How many grids would you like to play with?");
-	for(var i = 0; i < num; i++) {
-		$("<div class='unit'></div>").appendTo(".container");
-	}
-
+		for(var i = 0; i < 600; i++) {
+			$("<div class='unit'>O</div>").appendTo(".container");
+		}
+	
 });
 
 
